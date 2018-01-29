8621c163873a32d51fb74983327b744db079f99e
Flaki <git@flaki.hu>
2018-01-29 04:21:15 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,91 @@
+'use strict'
+
+// Dependencies
+const express = require('express')
+
+
+// Init returns an Express server module
+module.exports = init
+
+
+// Load configuratopn
+const links = setup()
+
+// Init server
+const server = init()
+
+
+// Serve on :3010
+server.listen(3010)
+
+
+
+function setup() {
+  const fs = require('fs'),
+        path = require('path')
+
+  let links = new Map()
+
+  try {
+    let inifile = fs.readFileSync(path.join(__dirname, '/data/links.ini')).toString()
+
+    // Break up to lines
+    inifile.split('\n')
+      // Skip empty & comment lines
+      .filter(l => l.trim() !== '' && l.trim()[0] !== '#')
+      // Set up slug => URL mappings and store them as "links"
+      .map(l => {
+        let [,label, url] = l.match(/^([\w\-]+)\:?\s+(.+)$/, 2)
+
+        // MicroCanvas examples root path
+        url = url.replace('<microcanvas-examples>', 'https://clouduboy.github.io/microcanvas/examples/')
+        links.set(label, url)
+      })
+
+      console.log(`Loaded ${links.size} link mappings`)
+      return links
+  }
+  catch (e) {
+    console.error(`No links.ini found. (${e.toString()})`)
+    // No links.ini
+  }
+}
+
+// Create a virtual host that will redirect incoming requests to the editor
+function init() {
+  const slh = express()
+
+  // Find session id if supplied
+  slh.param('slug', function(req, res, next, slug) {
+    // Check against loaded predefined shortlinks
+    if (links.has(slug)) {
+      // Redirect to predefined shortlink target
+      // console.log('redirecting to ', links.get(slug))
+      return res.redirect(links.get(slug))
+    }
+
+    // Check if provided slug is a valid session id
+    if (!slug.match(/^\w+\-\w+\-\w+$/)) {
+      // Unknown slug (not pre-defined and not a session id)
+      // We don't handle the request
+      return next()
+    }
+
+    // Redirect to editor
+    res.redirect(`https://editor.clouduboy.org/${slug}`)
+    // console.log("Shortlink: ", slug)
+  })
+
+
+  // Redirect session id-requests to the editor (handled by param)
+  slh.get('/:slug', () => {})
+
+  // All other requests redirect to main page
+  slh.get('*', (req, res) => {
+    res.redirect('https://clouduboy.org/')
+  })
+
+
+  // Create vhost
+  return slh
+}
